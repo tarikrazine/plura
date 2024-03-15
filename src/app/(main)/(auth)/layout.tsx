6b15f1d6f30255ee9571eeaa632b71d52dcd5b18
tarik razine <tarikrazine.com@gmail.com>
2024-03-15 01:14:32 +0000

    feat: project init

---
+++ src/app/(main)/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <main className="h-full flex justify-center items-center">{children}</main>;
+}
---
+++ src/app/(marketplace)/page.tsx
@@ -0,0 +1,5 @@
+export default  function MarketplacePage() {
+    return (
+        <div>Marketplace</div>
+    )
+}
